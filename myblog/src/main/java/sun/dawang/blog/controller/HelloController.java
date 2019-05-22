361f49a2bb4097414770b8992bf8e524749fe06c
sundawang <1170280085@qq.com>
2019-05-22 14:34:48 +0800

    myblog

---
+++ myblog/src/main/java/sun/dawang/blog/controller/HelloController.java
@@ -0,0 +1,13 @@
+package sun.dawang.blog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
