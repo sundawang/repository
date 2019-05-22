8783dd9a0e83fcab633a90d0edd65b6ebf4e9049
sundawang <1170280085@qq.com>
2019-05-22 14:47:39 +0800

    测试代码

--- myblog/src/main/java/sun/dawang/blog/controller/HelloController.java
+++ myblog/src/main/java/sun/dawang/blog/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String hello(){
-        System.out.println("测试代码");
+        System.out.println("测试代码1");
         return "hello world";
     }
 }
